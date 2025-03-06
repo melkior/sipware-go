b281787693e9cf446fd656d73f06655d972673b9
sipware <sipware@demokracija.hr>
2025-03-06 23:42:20 +0100

    add transaction: trans/tr.go

---
+++ trans/tr.go
@@ -0,0 +1,40 @@
+package trans
+
+import (
+	"sync"
+	"sipware/message"
+)
+
+type SipwareTr struct {
+        wg *sync.WaitGroup
+        ch chan message.Msg
+}
+
+func New() SipwareTr {
+	tr := SipwareTr {
+		wg: &sync.WaitGroup{},
+		ch: make(chan message.Msg),
+	}
+	return tr
+}
+
+func (tr SipwareTr) Add(i int) {
+	tr.wg.Add(i)
+}
+
+func (tr SipwareTr) Done() {
+	tr.wg.Done()
+}
+
+func (tr SipwareTr) Wait() {
+	tr.wg.Wait()
+}
+
+func (tr SipwareTr) Read() chan message.Msg {
+	return tr.ch
+}
+
+func (tr SipwareTr) Write(m message.Msg) {
+	tr.ch <- m
+}
+
