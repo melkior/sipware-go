248499c293eea0cba1cd605a77f39f7bbd2aed46
sipware <sipware@demokracija.hr>
2025-03-08 14:01:14 +0100

    api/ua changes

--- api/ua.go
+++ api/ua.go
@@ -6,17 +6,17 @@ import (
 )
 
 type Ua interface {
-        Name() string
-        Print()
-        Wait()
+	Name() string
+	Print()
+	Wait()
 	Open(string)
 	Exit() chan os.Signal
-        Connect()
-        Done()
-        Destroy(exiting bool)
-        Read([]byte) (n int, err error)
-        Write(message.Msg) (err error)
-        Request(message.Msg, func(message.Msg) error) error
-        Reply(message.Msg)
-        Register(interface{})
+	Connect(string)
+	Done()
+	Destroy(exiting bool)
+	Read([]byte) (n int, err error)
+	Write(message.Msg) (err error)
+	Request(message.Msg, func(message.Msg) error) error
+	Reply(message.Msg)
+	Register(interface{})
 }
