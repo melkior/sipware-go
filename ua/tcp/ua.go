fd3b5b95d8e5166544433bf840a8c8f3cc6d2afa
sipware <sipware@demokracija.hr>
2025-03-08 12:01:18 +0100

    cfg

--- ua/tcp/ua.go
+++ ua/tcp/ua.go
@@ -19,36 +19,6 @@ import (
 	"github.com/melkior/sipware-go/queues"
 )
 
-type User struct {
-	DisplayName string
-	Email string
-	Alias string
-}
-
-type RegisterConfig struct {
-        From string `json:"From"`
-        To string `json:"To"`
-        Method string `json:"Method"`
-        Alias string `json:"Alias"`
-        Password string `json:"Password"`
-}
-
-type ContactConfig struct {
-        File string `json:"contact"`
-        Value string `json:"value"`
-        Expires string `json:"expires"`
-}
-
-type CacheConfig struct {
-        Contact ContactConfig `json:"contact"`
-}
-
-type Config struct {
-        Open string `json:"open"`
-        Register RegisterConfig `json:"register"`
-        Cache CacheConfig `json:"cache"`
-}
-
 type SipwareTcpUa struct {
 	api.Ua
 	ctx context.Context
